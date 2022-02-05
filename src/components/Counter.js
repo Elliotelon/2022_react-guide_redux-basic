45fce364b4bc1c42cfdb260a291a447d0875684e
kimminkyu <kmk50009@naver.com>
2022-02-06 02:50:52 +0900

    Divide Code File

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,6 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch, connect } from "react-redux";
-import { counterActions } from "../store/index";
+import { counterActions } from "../store/counter";
 import { Component } from "react";
 
 const Counter = () => {
